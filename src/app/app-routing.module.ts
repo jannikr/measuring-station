0b1c8bffb9a7a8c158686c5ac34d5855d7b8fa57
Jan Rueckert <jan.nr@live.de>
2021-11-16 21:03:08 +0100

    Create records component #17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecordsComponent } from './records/records.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecordsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
