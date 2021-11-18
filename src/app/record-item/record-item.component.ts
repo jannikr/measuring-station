ec9e8d33fcee52d6545e198457b64ec6bfb4893a
Jan Rueckert <jan.nr@live.de>
2021-11-18 13:46:40 +0100

    Create new record-item component #24

---
+++ src/app/record-item/record-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-item',
+  templateUrl: './record-item.component.html',
+  styleUrls: ['./record-item.component.scss']
+})
+export class RecordItemComponent  {
+
+  constructor() { }
+
+
+
+}
