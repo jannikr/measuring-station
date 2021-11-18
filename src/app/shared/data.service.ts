a1425324e02b364eb448dfce0c62b3f4a4c8c6af
Jan Rueckert <jan.nr@live.de>
2021-11-18 19:44:02 +0100

    Add HttpClientModule in order to subscribe to the database #18

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,29 +1,54 @@
 import {Injectable} from '@angular/core';
 import {Record} from "./record.model";
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  // Todo: Add url from database
+  apiUrl = ""
+
   records: Record[] = [
-    new Record('MK76Y', new Date(2006,5,26), 42, 33, -9),
-    new Record('NV140', new Date(2006,5,28), 40, 40, 0)
+    new Record('MK76Y', new Date(2006, 5, 26), 42, 33, -9),
+    new Record('NV140', new Date(2006, 5, 28), 40, 40, 0)
   ]
 
-  constructor() { }
-
-  getAllRecords(){
-    return this.records
+  constructor(private _http: HttpClient) {
   }
 
-  getRecord(id: string){
+  getAllRecords(): Observable<Record[]> {
+    return this._http.get<Record[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError<Record[]>('getAllRecords', this.records))
+      )
+  }
+
+  getRecord(id: string) {
     return this.records.find(item => item.id !== id)
   }
 
-  updateRecord(updateRecord: Record){
+  updateRecord(updateRecord: Record) {
     let indexOfRecord: number;
     indexOfRecord = this.records.indexOf(<Record>this.getRecord(updateRecord.id));
     this.records[indexOfRecord] = updateRecord
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
