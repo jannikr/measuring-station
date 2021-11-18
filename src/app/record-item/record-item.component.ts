b59fe05310eaef04ac098fd428688dd10f6bf002
Jan Rueckert <jan.nr@live.de>
2021-11-18 17:37:00 +0100

    Transfer record-item specific parts to new component and minor improvements #24

--- src/app/record-item/record-item.component.ts
+++ src/app/record-item/record-item.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { Record} from "../shared/record.model";
 
 @Component({
   selector: 'app-record-item',
   templateUrl: './record-item.component.html',
   styleUrls: ['./record-item.component.scss']
 })
-export class RecordItemComponent  {
+export class RecordItemComponent{
+
+  // @ts-ignore
+  @Input() record: Record
 
   constructor() { }
-
-
 
 }
--- src/app/records/records.component.ts
+++ src/app/records/records.component.ts
@@ -1,20 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import { Record} from "../shared/record.model";
 import {DataService} from "../shared/data.service";
-import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
   styleUrls: ['./records.component.scss'],
-  providers: [DatePipe]
 })
 export class RecordsComponent implements OnInit{
 
-  records: Record[] | undefined
+  // @ts-ignore
+  records: Record[]
 
   // inject DataService
-  constructor(private datePipe: DatePipe, private dataService: DataService) { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.records = this.dataService.getAllRecords()
