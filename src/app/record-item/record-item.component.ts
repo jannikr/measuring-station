0c878c6fcdcbde09e145c165ddb339ef45a6dffe
Jan Rueckert <jan.nr@live.de>
2021-12-01 12:27:02 +0100

    Add logic to controller #5

--- src/app/record-item/record-item.component.ts
+++ src/app/record-item/record-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Record} from "../shared/record.model";
 
 @Component({
@@ -6,11 +6,29 @@ import { Record} from "../shared/record.model";
   templateUrl: './record-item.component.html',
   styleUrls: ['./record-item.component.scss']
 })
-export class RecordItemComponent{
+export class RecordItemComponent implements OnInit{
 
   // @ts-ignore
   @Input() record: Record
+  public variance: String | undefined
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.showDeviation()
+  }
+
+  // Todo: Check if this is executed when new data appears
+  showDeviation() {
+    if (this.record.actual <= 0.9 * this.record.target) {
+      console.log("Now critical!")
+      this.variance = "critical"
+    } else if (this.record.actual >= 0.05 * this.record.target + this.record.target) {
+      console.log("Now positive!")
+      this.variance = "positive"
+    } else {
+      this.variance = "not critical"
+    }
+  }
 
 }
